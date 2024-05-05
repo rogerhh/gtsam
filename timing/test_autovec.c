60a6aa4475b47ad5f2ec6a5b9debc281ae1990c5
Roger Hsiao <rogerhh@umich.edu>
2024-05-05 16:47:38 -0700

    matmul2

--- timing/test_autovec.c
+++ timing/test_autovec.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 
-void matmul(int dim_I, int dim_J, int dim_K,
+void matmul1(int dim_I, int dim_J, int dim_K,
             float* A, float* B, float* C,
             int stride_A, int stride_B, int stride_C) {
   float* Acol = A;
   float* Ccol = C;
+  float AB_scale_factor = 1.0;
   for(size_t i = 0; i < dim_I; i++) {
     float* Bcol = B;
     for(size_t k = 0; k < dim_K; k++) {
       for(size_t j = 0; j < dim_J; j++) {
-        Ccol[j] += Acol[k] * B[j];
+        Ccol[j] += AB_scale_factor * Acol[k] * B[j];
       }
       Bcol += stride_B;
     }
     Acol += stride_A;
     Ccol += stride_C;
+  }
+}
+
+void matmul2(int dim_I, int dim_J, int dim_K,
+            float* A, float* B, float* C,
+            int stride_A, int stride_B, int stride_C) {
+  float AB_scale_factor = 1.0;
+  for(size_t i = 0; i < dim_I; i++) {
+    for(size_t j = 0; j < dim_J; j++) {
+      for(size_t k = 0; k < dim_K; k++) {
+          C[i * stride_C + j] += AB_scale_factor * A[i * stride_A + k] * B[j * stride_B + k];
+      }
+    }
   }
 }
 
@@ -25,7 +39,8 @@ int main() {
     float b[1024] = {1, 2, 3, 4, 5, };
     float c[1024] = {1, 2, 3, 4, 5, };
 
-    matmul(5, 5, 5, a, b, c, 10, 10, 10);
+    matmul1(5, 5, 5, a, b, c, 10, 10, 10);
+    matmul2(5, 5, 5, a, b, c, 10, 10, 10);
 
     for(int i = 0; i < LEN; i++) {
 	printf("%f ", a[i]);
