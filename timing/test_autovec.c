8e89423e7696042ff3ddc575631760748cf011b9
Roger Hsiao <rogerhh@umich.edu>
2024-05-05 16:51:50 -0700

    matmul2

--- timing/test_autovec.c
+++ timing/test_autovec.c
@@ -22,13 +22,19 @@ void matmul1(int dim_I, int dim_J, int dim_K,
 void matmul2(int dim_I, int dim_J, int dim_K,
             float* A, float* B, float* C,
             int stride_A, int stride_B, int stride_C) {
+  float* Acol = A;
+  float* Ccol = C;
   float AB_scale_factor = 1.0;
   for(size_t i = 0; i < dim_I; i++) {
-    for(size_t j = 0; j < dim_J; j++) {
-      for(size_t k = 0; k < dim_K; k++) {
-          C[i * stride_C + j] += AB_scale_factor * A[i * stride_A + k] * B[j * stride_B + k];
+    for(size_t k = 0; k < dim_K; k++) {
+      float* Bcol = B + k;
+      for(size_t j = 0; j < dim_J; j++) {
+        Ccol[j] += AB_scale_factor * Acol[k] * (*Bcol);
+        Bcol += stride_B;
       }
     }
+    Acol += stride_A;
+    Ccol += stride_C;
   }
 }
 
