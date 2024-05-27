53fe0c71f63c6c74cdc2c5737244737c04526774
Roger Hsiao <rogerhh@umich.edu>
2024-05-27 01:31:14 -0700

    Atb

--- timing/test_multithreaded_incremental_dataset_no_values_gpu.cu
+++ timing/test_multithreaded_incremental_dataset_no_values_gpu.cu
@@ -19,6 +19,29 @@
 
 using namespace std;
 
+void printDeviceVals(float* d_vals, int n, const string& name, const string& type) {
+    if(type == "int") {
+        int* h_vals = (int*)malloc(n * sizeof(int));
+        cudaMemcpy(h_vals, d_vals, n * sizeof(int), cudaMemcpyDeviceToHost);
+        printf("%s: ", name.c_str());
+        for(int i = 0; i < n; i++) {
+            printf("%d ", h_vals[i]);
+        }
+        printf("\n");
+        free(h_vals);
+    }
+    else if(type == "float") {
+        float* h_vals = (float*)malloc(n * sizeof(float));
+        cudaMemcpy(h_vals, d_vals, n * sizeof(float), cudaMemcpyDeviceToHost);
+        printf("%s: ", name.c_str());
+        for(int i = 0; i < n; i++) {
+            printf("%f ", h_vals[i]);
+        }
+        printf("\n");
+        free(h_vals);
+    }
+}
+
 int main(int argc, char** argv) {
     // Initialize cuSPARSE and cuSOLVER
     cusparseHandle_t cusparseHandle = NULL;
@@ -46,12 +69,27 @@ int main(int argc, char** argv) {
         int** node_factor_width = step_node_factor_width[step];
         int*** node_factor_ridx = step_node_factor_ridx[step];
 
-        // Construct A matrix from factors
+        // Host data
         vector<int> h_csrRowPtrA = {0};
         vector<int> h_csrColIndA;
         vector<float> h_csrValA;
-
+        vector<int> h_csrRowPtrAT = {0};
+        vector<int> h_csrColIndAT;
+        vector<float> h_csrValAT;
         vector<float> h_b;
+
+        // Device data
+        int* d_csrRowPtrA;
+        int* d_csrColIndA;
+        float* d_csrValA;
+        int* d_csrRowPtrAT;
+        int* d_csrColIndAT;
+        float* d_csrValAT;
+        float* d_b;
+        float* d_ATb;
+        float* d_x;
+        size_t bufferSize;
+        void* buffer = NULL;
 
         set<int> ridx_set;
         map<int, int> remapped_ridx;
@@ -112,6 +150,68 @@ int main(int argc, char** argv) {
             printf("%f ", h_csrValA[i]);
         }
         printf("\n");
+
+        // Transpose A
+        h_csrRowPtrAT.resize(remapped_ridx.size() + 1, 0);
+        for(int i = 0; i < h_csrColIndA.size(); i++) {
+            h_csrRowPtrAT[h_csrColIndA[i] + 1]++;
+        }
+        for(int i = 1; i < h_csrRowPtrAT.size(); i++) {
+            h_csrRowPtrAT[i] += h_csrRowPtrAT[i - 1];
+        }
+        h_csrColIndAT.resize(h_csrColIndA.size());
+        h_csrValAT.resize(h_csrValA.size());
+        for(int i = 0; i < h_csrRowPtrA.size() - 1; i++) {
+            for(int j = h_csrRowPtrA[i]; j < h_csrRowPtrA[i + 1]; j++) {
+                int col = h_csrColIndA[j];
+                int idx = h_csrRowPtrAT[col]++;
+                h_csrColIndAT[idx] = i;
+                h_csrValAT[idx] = h_csrValA[j];
+            }
+        }
+
+        // Convenience variables
+        // A is m x n, AT is n x m, b is m x 1, ATb is n x 1, x is n x 1
+        int m = h_csrRowPtrA.size() - 1;
+        int n = h_csrRowPtrAT.size() - 1;
+        int one = 1, zero = 0;
+
+        // Matrix descriptors
+        cusparseMatDescr_t descrA;
+        cusparseCreateMatDescr(&descrA);
+        cusparseMatDescr_t descrAT;
+        cusparseCreateMatDescr(&descrAT);
+        cusparseDnVecDescr_t descrb;
+        cusparseCreateDnVec(&descrb);
+        cusparseDnVecDescr_t descrATb;
+        cusparseCreateDnVec(&descrATb);
+
+        // Device memory allocation
+        cudaMalloc(&d_csrRowPtrA, h_csrRowPtrA.size() * sizeof(int));
+        cudaMalloc(&d_csrColIndA, h_csrColIndA.size() * sizeof(int));
+        cudaMalloc(&d_csrValA, h_csrValA.size() * sizeof(float));
+        cudaMalloc(&d_csrRowPtrAT, h_csrRowPtrAT.size() * sizeof(int));
+        cudaMalloc(&d_csrColIndAT, h_csrColIndAT.size() * sizeof(int));
+        cudaMalloc(&d_csrValAT, h_csrValAT.size() * sizeof(float));
+        cudaMalloc(&d_b, h_b.size() * sizeof(float));
+        cudaMalloc(&d_ATb, n * sizeof(float));
+        cudaMalloc(&d_x, n * sizeof(float));
+
+        // Device memory copy
+        cudaMemcpy(d_csrRowPtrA, h_csrRowPtrA.data(), h_csrRowPtrA.size() * sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_csrColIndA, h_csrColIndA.data(), h_csrColIndA.size() * sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_csrValA, h_csrValA.data(), h_csrValA.size() * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_csrRowPtrAT, h_csrRowPtrAT.data(), h_csrRowPtrAT.size() * sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_csrColIndAT, h_csrColIndAT.data(), h_csrColIndAT.size() * sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_csrValAT, h_csrValAT.data(), h_csrValAT.size() * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_b, h_b.data(), h_b.size() * sizeof(float), cudaMemcpyHostToDevice);
+
+        // Compute ATb
+        cusparseSpMV_bufferSize(cusparseHandle, CUSPARSE_OPERATION_NON_TRANSPOSE,
+                                &one, descrAT, descrb, &zero, descrATb, 
+                                CUDA_R_32F, CUSPARSE_CSRMV_ALG1, &bufferSize);
+
+        printf("bufferSize = %lu\n", bufferSize);
 
 	end = clock();
 	double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
